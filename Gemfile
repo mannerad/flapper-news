88c89a52c485f17e990dfe8d8d67672ef6f3a6ec
Alex <mannerad@muohio.edu>
2015-10-27 04:15:49 -0500

    Wired frontend to backend.  Everything works.

--- Gemfile
+++ Gemfile
@@ -14,8 +14,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'angular_rails_csrf'
 gem 'angular-rails-templates'
-gem 'responders', '~> 2.0'
+gem 'responders', '~> 2.1'
 # gem 'sprockets', '2.12.3'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/home/mainController.js
+++ app/assets/javascripts/home/mainController.js
@@ -6,17 +6,15 @@ app.controller('MainController', [
 		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){ return; }
-			$scope.posts.push({
+			posts.create({
 				title: $scope.title, 
-				link: $scope.link, 
-				upvotes: 0,
-				comments: []
+				link: $scope.link
 			});
 			$scope.title = '';
 			$scope.link = '';
 		};
 		$scope.incrementUpvotes = function(post){
-			post.upvotes += 1;
+			posts.upvote(post);
 		};
 	}
 ]);
